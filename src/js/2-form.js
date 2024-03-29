de3d3c2c56d1e57c5ea87694ddd346eeb93d0841
d-sofiika <soniadrozd2709@gmail.com>
2024-03-29 23:46:48 +0100

    done task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,41 @@
-console.log("Form");
+const storageKey = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const textEmail = document.querySelector(".email-input")
+const textarea = form.querySelector(".message");
+
+
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("input", onInput);
+
+
+function onInput(event) {
+    const save = {
+        email: form.email.value.trim(),
+        message: form.message.value.trim(),
+    };
+    localStorage.setItem(storageKey, JSON.stringify(save));
+   
+}
+
+const savedMessage = localStorage.getItem(storageKey);
+
+if (savedMessage) {
+    const { email, message } = JSON.parse(savedMessage);
+    form.email.value = email;
+    form.message.value = message;
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    if (form.email.value === "" || form.message.value === "") {
+        alert("Усі поля форми мають бути заповнені");
+        console.log("Error");
+    } else {
+    console.log("send");
+    console.log(localStorage.getItem(storageKey));
+    event.currentTarget.reset();
+    localStorage.removeItem(storageKey);}
+}
